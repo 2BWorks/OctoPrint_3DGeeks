4d4a8d07d8b50a7e00a93296c5bc88d15c2f24f9
Toby Bonhof <apps@2b.work>
2020-06-22 17:06:59 +0200

    deprecated

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-zip -r octoprint_geeks3d.zip .
-
--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-zip -r plugin.zip .
-/Users/snoep/OctoPrint/venv/bin/pip install plugin.zip
-rm plugin.zip
-/Users/snoep/OctoPrint/venv/bin/octoprint serve
